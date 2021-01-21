66a8a2d1e12a2d17b7397120b5025c9f8bc0da81
annielly <annielly@ufrn.edu.br>
2021-01-21 11:12:15 -0300

    sobre

---
+++ sobre.c
@@ -0,0 +1,43 @@
+/*************************************************/
+/*  Universidade Federal do Rio Grande do Norte  */
+/*      Centro de Ensino Superior do Seridó      */
+/*   Departamento de Computação e Tecnologia     */
+/*       Disciplina DCT1106 -- Programação       */
+/*      Centro de Ensino Superior do Serido      */
+/*   Departamento de Computacao e Tecnologia     */
+/*       Disciplina DCT1106 -- Programacao       */
+/*    Projeto Sistema de Controle de Estoques    */
+/*     Developed by @anniellyfs - Jan, 2021      */
+/*     Developed by @           - Jan, 2021      */
+/*************************************************/
+
+#include <stdio.h>
+void menuSobre(void);
+int main(void) {
+    menuSobre();
+    return 0;
+}
+
+void menuSobre(void){
+  printf("=====================================================\n");
+  printf("||   Universidade Federal do Rio Grande do Norte   ||\n");
+  printf("||       Centro de Ensino Superior do Seridó       ||\n");
+  printf("||     Departamento de Computação e Tecnologia     ||\n");
+  printf("||        Disciplina DCT1106 -- Programação        ||\n");
+  printf("||     Projeto Sistema de Controle de Estoques     ||\n");
+  printf("||      Developed by @anniellyfs - Jan, 2021       ||\n");
+  printf("||         Contato: annielly@ufrn.edu.br           ||\n");
+  printf("=====================================================\n");
+  printf("=====================================================\n");
+  printf("||                                                 ||\n");
+  printf("||         SISTEMA DE CONTROLE DE ESTOQUES         ||\n");
+  printf("||                                                 ||\n");
+  printf("||     Programa desenvolvido para avaliação na     ||\n");
+  printf("||  disciplina DCT1106 - Programação. O software   ||\n");
+  printf("||  contém funcionalidades básicas para controle   ||\n");
+  printf("||   de inventário de uma empresa, facilitando a   ||\n");
+  printf("|| administração de estoques de variados produtos. ||\n");
+  printf("||                                                 ||\n");
+  printf("=====================================================\n");
+  getchar();
+}
