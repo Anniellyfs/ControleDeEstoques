20c767fd463d02dd7b47ab59e0f3a36271651f09
annielly <annielly@ufrn.edu.br>
2021-05-07 10:10:48 -0300

    tela sobre

--- sobre.c
+++ sobre.c
@@ -21,23 +21,25 @@ int main(void) {
 
 void menuSobre(void){
   printf("=====================================================\n");
+  printf("||                                                 ||\n");
   printf("||   Universidade Federal do Rio Grande do Norte   ||\n");
-  printf("||       Centro de Ensino Superior do Seridó       ||\n");
-  printf("||     Departamento de Computação e Tecnologia     ||\n");
-  printf("||        Disciplina DCT1106 -- Programação        ||\n");
+  printf("||       Centro de Ensino Superior do Serido       ||\n");
+  printf("||     Departamento de Computacao e Tecnologia     ||\n");
+  printf("||        Disciplina DCT1106 -- Programacao        ||\n");
   printf("||     Projeto Sistema de Controle de Estoques     ||\n");
   printf("||      Developed by @anniellyfs - Jan, 2021       ||\n");
-  printf("||         Contato: annielly@ufrn.edu.br           ||\n");
+  printf("||     Contato: annielly.sousa.060@ufrn.edu.br     ||\n");
+  printf("||                                                 ||\n");
   printf("=====================================================\n");
   printf("=====================================================\n");
   printf("||                                                 ||\n");
   printf("||         SISTEMA DE CONTROLE DE ESTOQUES         ||\n");
   printf("||                                                 ||\n");
-  printf("||     Programa desenvolvido para avaliação na     ||\n");
-  printf("||  disciplina DCT1106 - Programação. O software   ||\n");
-  printf("||  contém funcionalidades básicas para controle   ||\n");
-  printf("||   de inventário de uma empresa, facilitando a   ||\n");
-  printf("|| administração de estoques de variados produtos. ||\n");
+  printf("||     Programa desenvolvido para avaliacao na     ||\n");
+  printf("||  disciplina DCT1106 - Programacao. O software   ||\n");
+  printf("||  contem funcionalidades basicas para controle   ||\n");
+  printf("||   de inventario de uma empresa, facilitando a   ||\n");
+  printf("|| administracao de estoques de variados produtos. ||\n");
   printf("||                                                 ||\n");
   printf("=====================================================\n");
   getchar();
