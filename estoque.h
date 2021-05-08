e0e648a15848b42accbef46135b0b4522dc1f355
annielly <annielly@ufrn.edu.br>
2021-05-08 19:53:32 -0300

    assinaturas estoque

---
+++ estoque.h
@@ -0,0 +1,7 @@
+//ASSINATURA MENU ESTOQUE
+char menuEstoque(void);
+void navegacaoEstoque(void);
+void cadastrarEstoque(void);
+void altualizarEstoque(void);
+void pesquisarEstoque(void);
+void excluirEstoque(void);
