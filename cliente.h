d32f0e56d3d53c3c402904679a4cc94f786669bb
annielly <annielly@ufrn.edu.br>
2021-05-08 19:28:53 -0300

    assinaturas cliente

---
+++ cliente.h
@@ -0,0 +1,7 @@
+//ASSINATURA MENU CLIENTE
+char menuCliente(void);
+void navegacaoCliente(void);
+void cadastrarCliente(void);
+void altualizarCliente(void);
+void pesquisarCliente(void);
+void excluirCliente(void);
