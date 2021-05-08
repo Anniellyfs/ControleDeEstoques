81e385b67f4a61dd77f2103f28f0066eeaa85ce7
annielly <annielly@ufrn.edu.br>
2021-05-08 19:43:55 -0300

    assinaturas saidas

---
+++ saida.h
@@ -0,0 +1,7 @@
+//ASSINATURA MENU SAIDA
+char menuSaida(void);
+void navegacaoSaida(void);
+void cadastrarSaida(void);
+void altualizarSaida(void);
+void pesquisarSaida(void);
+void excluirSaida(void);
