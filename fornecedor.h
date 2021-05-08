e826e0e97baf4f6770cf4cd359251d762e19daca
annielly <annielly@ufrn.edu.br>
2021-05-08 19:19:23 -0300

    assinaturas fornecdor

--- fornecedor.h
+++ fornecedor.h
@@ -1,3 +1,7 @@
 //ASSINATURA MENU FORNECEDOR
 char menuFornecedor(void);
 void navegacaoFornecedor(void);
+void cadastrarFornecedor(void);
+void altualizarFornecedor(void);
+void pesquisarFornecedor(void);
+void excluirFornecedor(void);
