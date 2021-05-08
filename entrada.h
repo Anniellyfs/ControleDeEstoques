017d9fe331adad6475ab39d18acb5199d72d9ec5
annielly <annielly@ufrn.edu.br>
2021-05-08 19:38:47 -0300

    assinaturas entrada

---
+++ entrada.h
@@ -0,0 +1,7 @@
+//ASSINATURA MENU ENTRADA
+char menuEntrada(void);
+void navegacaoEntrada(void);
+void cadastrarEntrada(void);
+void altualizarEntrada(void);
+void pesquisarEntrada(void);
+void excluirEntrada(void);
