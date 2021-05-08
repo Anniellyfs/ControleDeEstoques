712f7340d7f0f071f4bd8416a83f0c636ae871bc
annielly <annielly@ufrn.edu.br>
2021-05-08 18:55:32 -0300

    telas foenecedor

---
+++ fornecedor.c
@@ -0,0 +1,65 @@
+//BIBLIOTECAS NECESSÁRIAS PARA O PROGRAMA
+#include <stdio.h>
+#include <stdlib.h>
+#include "fornecedor.h"
+
+
+//MENU PRINCIPAL
+char menuFornecedor(void){
+  char op;
+  system("cls || clear");
+  printf("**********************************************\n");
+  printf("*                                            *\n");
+  printf("*              MENU FORNECEDOR               *\n");
+  printf("*                                            *\n");
+  printf("*     __________________________________     *\n");
+  printf("*     |                                |     *\n");
+  printf("*     |                                |     *\n");
+  printf("*     |       [1]  Cadastrar           |     *\n");
+  printf("*     |       [2]  Atualizar           |     *\n");
+  printf("*     |       [3]  Pesquisar           |     *\n");
+  printf("*     |       [4]  Excluir             |     *\n");
+  printf("*     |                                |     *\n"); 
+  printf("*     |       [0]  Sair                |     *\n");
+  printf("*     |                                |     *\n");
+  printf("*     |________________________________|     *\n");
+  printf("*                                            *\n");
+  printf("*                                            *\n");
+  printf("**********************************************\n");
+  scanf("%s", op);
+  getchar();
+return op;
+}
+
+void navegacaoFornecedor(void){
+    char op;
+    op = menuFornecedor();
+    do{
+        switch (op)
+        {
+        case '1':
+            cadastrarFornecedor();
+            break;
+        
+        case '2':
+            altualizarFornecedor();
+            break;
+
+        case '3':
+            pesquisarFornecedor();
+            break;
+        
+        case '4':
+            excluirFornecedor();
+            break;
+
+        default:
+            printf("A OPCAO DIGITADA EH INVALIDA!");
+            printf("Digite novamente!");
+            break;
+        }
+    }while(op != '0');
+    getchar();
+    return op;
+
+}
---
+++ fornecedor.h
@@ -0,0 +1,3 @@
+//ASSINATURA MENU FORNECEDOR
+char menuFornecedor(void);
+void navegacaoFornecedor(void);
