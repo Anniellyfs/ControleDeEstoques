0d22f720260e064bf0c374e49c5df8ab25641407
annielly <annielly@ufrn.edu.br>
2021-05-12 10:52:31 -0300

    criação da struct

--- fornecedor.c
+++ fornecedor.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "fornecedor.h"
 
+typedef struct fornecedor Fornecedor;
 
 //MENU PRINCIPAL
 char menuFornecedor(void){
--- fornecedor.h
+++ fornecedor.h
@@ -5,3 +5,14 @@ void cadastrarFornecedor(void);
 void altualizarFornecedor(void);
 void pesquisarFornecedor(void);
 void excluirFornecedor(void);
+
+//ESTRUTURA FORNECEDOR
+typedef struct fornecedor Fornecedor;
+
+struct fornecedor{
+  char data[10];
+  int cnpj;
+  char fornecedor[30];
+  char cidade[30];
+  char estado[2];        
+};
